1dfcd291398c4fed438602f13e5a4c7d01f6fef3
Alexander Boldyrev <al.boldyrev1@gmail.com>
2019-08-18 19:02:39 +0300

    New sequences

---
+++ src/main/resources/database/sequences.sql
@@ -0,0 +1,9 @@
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_dialog_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_dialogMaxStates_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_dialogPhrase_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_dialogToState_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_message_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_phrasePair_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_settings_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_user_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS lingvobot.lingvobot_userDialog_sequence START 1;
