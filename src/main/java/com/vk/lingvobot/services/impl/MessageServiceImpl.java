7b0c4f0b9e5b525fef8ef8c0b4b1485016b399f1
Alexander Boldyrev <al.boldyrev1@gmail.com>
2020-01-10 12:20:45 +0300

    SetActivity

--- src/main/java/com/vk/lingvobot/services/impl/MessageServiceImpl.java
+++ src/main/java/com/vk/lingvobot/services/impl/MessageServiceImpl.java
@@ -5,11 +5,14 @@ import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.messages.Keyboard;
+import com.vk.lingvobot.util.Activity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import com.vk.lingvobot.services.MessageService;
 
 @Component
@@ -25,22 +28,33 @@ public class MessageServiceImpl implements MessageService {
     @Autowired
     private UserInfoServiceImpl userInfo;
 
+    @Autowired
+    private Activity activity;
+
     private final Random random = new Random();
 
     public void sendMessageTextOnly(int userVkId, String message) {
 
+        int howMuchShouldActivityMessageBeDisplayed = activity.howMuchShouldActivityMessageBeDisplayed(message);
         String userDomain = userInfo.getUserDomain(groupActor, userVkId);
         try {
+            System.out.println("Lol, activity is working and we decided that with message " + message +
+                    " that swirling dots should be shown for: " + howMuchShouldActivityMessageBeDisplayed + " seconds.");
+            apiClient.messages().setActivity(groupActor).type("typing").userId(userVkId).peerId(userVkId).execute();
+            TimeUnit.SECONDS.sleep(howMuchShouldActivityMessageBeDisplayed);
             apiClient.messages().send(groupActor).peerId(userVkId).userIds(userVkId).message(message).randomId(random.nextInt()).domain(userDomain).execute();
+
         } catch (ApiException e) {
             log.error("Something wrong with API: " + e.getStackTrace());
         } catch (ClientException e) {
             log.error("Something wrong with CLIENT: " + e.getStackTrace());
+        } catch (InterruptedException e) {
+            log.error("I really do not know how sleep method can fail" + e.getStackTrace());
         }
 
     }
 
-    public void sendMessageWithTextAndAttachements(int userVkId, String message, String attachments) {
+    public void sendMessageWithTextAndAttachments(int userVkId, String message, String attachments) {
 
         String userDomain = userInfo.getUserDomain(groupActor, userVkId);
         try {
--- src/main/java/com/vk/lingvobot/services/impl/UserDialogServiceImpl.java
+++ src/main/java/com/vk/lingvobot/services/impl/UserDialogServiceImpl.java
@@ -6,10 +6,7 @@ import com.vk.lingvobot.repositories.DialogMaxStateRepository;
 import com.vk.lingvobot.repositories.DialogStateRepository;
 import com.vk.lingvobot.repositories.PhrasePairStateRepository;
 import com.vk.lingvobot.repositories.UserDialogRepository;
-import com.vk.lingvobot.services.MessageServiceKt;
-import com.vk.lingvobot.services.PhrasePairService;
-import com.vk.lingvobot.services.PhrasePairStateService;
-import com.vk.lingvobot.services.UserDialogService;
+import com.vk.lingvobot.services.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,6 +24,8 @@ public class UserDialogServiceImpl implements UserDialogService {
     private DialogStateRepository dialogStateRepository;
     @Autowired
     private MessageServiceKt messageServiceKt;
+    @Autowired
+    private MessageService messageService;
     @Autowired
     private DialogMaxStateRepository dialogMaxStateRepository;
     @Autowired
@@ -68,9 +67,9 @@ public class UserDialogServiceImpl implements UserDialogService {
         String dialogPhraseAttach = dialogPhrase.getAttach();
 
         if (dialogPhraseAttach == null) {
-            messageServiceKt.sendMessageTextOnly(groupActor, user.getVkId(), dialogPhraseValue);
+            messageService.sendMessageTextOnly(user.getVkId(), dialogPhraseValue);
         } else {
-            messageServiceKt.sendMessageWithTextAndAttachments(groupActor, user.getVkId(), dialogPhraseValue, dialogPhraseAttach);
+            messageService.sendMessageWithTextAndAttachments(user.getVkId(), dialogPhraseValue, dialogPhraseAttach);
         }
         log.info("Сообщение отправлено! ");
 
